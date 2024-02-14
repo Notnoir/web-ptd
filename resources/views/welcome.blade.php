a09a117d656fbb013473f73414fd31f9fa7f0dad
Notnoir <riza80448@gmail.com>
2024-02-14 14:50:58 +0700

    nambah juga

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,6 +12,8 @@
     Hello world!
     Hello world!
     Hello world!
+
+    nambah jugaaa
   </h1>
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js"></script>
