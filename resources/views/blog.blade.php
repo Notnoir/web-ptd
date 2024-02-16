affd049ed15683f20e91a8a6baf7c7cb68d80625
Notnoir <riza80448@gmail.com>
2024-02-17 01:11:31 +0700

    page about

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -16,8 +16,8 @@
     {{-- jumbotron --}}
     <section class="bg-center bg-no-repeat bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply">
         <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
-            <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Cyber Security Excellence, Your Ultimate Digital Guardian</h1>
-            <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+            <h1 class="mb-4 text-4xl font-bold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Cyber Security Excellence, Your Ultimate Digital Guardian</h1>
+            <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Empowering Digital Fortresses. Where Cyber Security Meets Innovation. Defend, Protect, and Secure in the Digital Age.</p>
             {{-- <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
                 <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
                     Get started
@@ -41,7 +41,7 @@
                 </div>
 
                 <div class="md:block hidden">
-                    <img src="/img/blog5.png" width="500px" alt="" />
+                    <img src="/img/blog6.png" width="500px" alt="" />
                 </div>
             </div>
         </div>
