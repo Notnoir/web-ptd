bcef184b751b93be6a41390c70dcfea510f726d7
Notnoir <riza80448@gmail.com>
2024-03-11 22:14:39 +0700

    edit page contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,28 +6,28 @@
             <div class="max-w-sm bg-gray-200 border border-white rounded-lg dark:bg-gray-800 dark:border-gray-700">
                 <div class="flex flex-col items-center pb-10 pt-4">
                     <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="/img/rico.jpeg" alt="Bonnie image"/>
-                    <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Rico Fadli Alfiansyah</h5>
+                    <h5 class="mb-1 md:text-xl text-sm font-medium text-gray-900 dark:text-white">Rico Fadli Alfiansyah</h5>
                     <span class="text-sm text-gray-500">15-2023-090</span>
                 </div>
             </div>
             <div class="max-w-sm bg-gray-200 border border-white rounded-lg dark:bg-gray-800 dark:border-gray-700">
                 <div class="flex flex-col items-center pb-10 pt-4">
                     <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="/img/riza.jpg" alt="Bonnie image"/>
-                    <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Riza Maulana Tamsirin</h5>
+                    <h5 class="mb-1 md:text-xl text-sm font-medium text-gray-900 dark:text-white">Riza Maulana Tamsirin</h5>
                     <span class="text-sm text-gray-500">15-2023-109</span>
                 </div>
             </div>
             <div class="max-w-sm bg-gray-200 border border-white rounded-lg dark:bg-gray-800 dark:border-gray-700">
                 <div class="flex flex-col items-center pb-10 pt-4">
                     <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="/img/mikel.jpg" alt="Bonnie image"/>
-                    <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Arsyad Effendi</h5>
+                    <h5 class="mb-1 md:text-xl text-sm font-medium text-gray-900 dark:text-white">Arsyad Effendi</h5>
                     <span class="text-sm text-gray-500">15-2023-107</span>
                 </div>
             </div>
             <div class="max-w-sm bg-gray-200 border border-white rounded-lg dark:bg-gray-800 dark:border-gray-700">
                 <div class="flex flex-col items-center pb-10 pt-4">
                     <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="/img/dimas.jpg" alt="Bonnie image"/>
-                    <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Fachrisan Dimas Narendra</h5>
+                    <h5 class="mb-1 md:text-xl text-sm font-medium text-gray-900 dark:text-white">Fachrisan Dimas Narendra</h5>
                     <span class="text-sm text-gray-500">15-2023-111</span>
                 </div>
             </div>
